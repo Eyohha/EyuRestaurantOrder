04190f696271d3b46d44753b9876c64477075b19
[eyohha] <76925136+Eyohha@users.noreply.github.com>
2021-11-17 21:38:37 -0500

    My Demo

--- src/main/java/com/abri/tech/orderservice/OrderserviceApplication.java
+++ src/main/java/com/abri/tech/orderservice/OrderserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello Amit");
 		SpringApplication.run(OrderserviceApplication.class, args);
 	}
 
